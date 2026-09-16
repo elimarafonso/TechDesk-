0522657ea756a06268102e322eb9b17df7423143
elimar_afonso <elimar_afonso@hotmail.com>
2026-09-16 09:54:02 -0300

    desenvolvimento dos testes unitarios, findall,findbyid funcionando

--- techdesk-chamados-api/src/test/java/com/techdesk/techdesk/categorias/service/CategoriaServiceTest.java
+++ techdesk-chamados-api/src/test/java/com/techdesk/techdesk/categorias/service/CategoriaServiceTest.java
@@ -1,8 +1,12 @@
 package com.techdesk.techdesk.categorias.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,30 +23,83 @@ import com.techdesk.techdesk.categorias.repository.CategoriaRepository;
 @ExtendWith(MockitoExtension.class)
 class CategoriaServiceTest {
 
-	@InjectMocks 
+	@InjectMocks
 	private CategoriaService categoriaService;
-	
+
 	@Mock
 	private CategoriaRepository categoriaRepository;
-
-
 
 	@Test
 	@DisplayName("Deve Retornar uma Lista de Categorias")
 	public void deveRetornarUmaListaDeCategorias() {
-		
+
 		Categoria categoriaManutencao = new Categoria(1L, "Manutencao", null);
-		Categoria categoriaRecepcao = new Categoria(1L, "Recepcao",null);
-		
+		Categoria categoriaRecepcao = new Categoria(2L, "Recepcao", null);
+
 		List<Categoria> categoriaList = new ArrayList<>();
 		categoriaList.add(categoriaManutencao);
 		categoriaList.add(categoriaRecepcao);
-		
-		
-		Mockito.when(categoriaRepository.findAll()).thenReturn(Collections.list(categoriaList));
-		List<CategoriaResponseDTO> allCategorias = categoriaService.findAll();
-		System.out.println(allCategorias);
 
+		Mockito.when(categoriaRepository.findAll()).thenReturn(categoriaList);
+
+		List<CategoriaResponseDTO> allCategory = categoriaService.findAll();
+
+		assertNotNull(allCategory);
+		assertEquals(2, allCategory.size());
+
+		assertEquals(1L, allCategory.get(0).id());
+		assertEquals("Manutencao", allCategory.get(0).nome());
+
+		assertEquals(2L, allCategory.get(1).id());
+		assertEquals("Recepcao", allCategory.get(1).nome());
+
+		Mockito.verify(categoriaRepository, Mockito.times(1)).findAll();
 	}
 
+	@Test
+	@DisplayName("Deve retornar uma lista vazia")
+	public void deveRetornarUmaListaVazia() {
+		
+		Mockito.when(categoriaService.findAll()).thenReturn(List.of());
+		
+		List<CategoriaResponseDTO> allCategory = categoriaService.findAll();
+		
+		assertTrue(allCategory.isEmpty());
+		Mockito.verify(categoriaRepository).findAll();
+	}
+	
+	
+	@Test
+	@DisplayName("Deve retornar uma Categoria")
+	public void deveRetornarUmaCategoria() {
+		
+		Categoria categoria = new Categoria(1L, "Desenvolvimento",null);
+		
+			
+		Mockito.when(categoriaRepository.findById(1L)).thenReturn(Optional.of(categoria));
+		
+		
+		CategoriaResponseDTO buscaCategoria =categoriaService.buscar(1L);
+		
+		assertNotNull(buscaCategoria);
+		
+		
+		assertEquals(1L, buscaCategoria.id());
+		assertEquals("Desenvolvimento", buscaCategoria.nome());
+		
+		Mockito.verify(categoriaRepository, Mockito.times(1)).findById(1L);
+		
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
